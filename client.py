2a54996b392a55e00470056925e73161b602da5f
magdss <62175061+magdss@users.noreply.github.com>
2020-12-15 19:14:09 +0100

    update

--- client.py
+++ client.py
@@ -1,14 +1,34 @@
 import socket
+import bitarray
 
 server_ip = ("127.0.0.1", 20001)
 Socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-while(True):
-    message = input("Podaj wiadomosc")
-    message = str.encode(message)
-    Socket.sendto(message, server_ip)
 
+operation = bitarray.bitarray(5)
+operation.setall(False)
+operation[4] = True
+response = bitarray.bitarray(4)
+response.setall(False)
+id = bitarray.bitarray(6)
+id.setall(False)
+data = bitarray.bitarray(10)
+data.setall(False)
+fill = bitarray.bitarray(4)
+fill.setall(False)
+control_sum = bitarray.bitarray(11)
+control_sum.setall(False)
 
+header = [operation, response, id, data, fill]
+
+message = bitarray.bitarray(endian='big')
+
+for i in range(5):
+    message += header[i]
+
+message = message.tobytes()
+
+Socket.sendto(message, server_ip)
 
 
 
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import bitarray
 
 local_ip = "127.0.0.1"
 port = 20001
@@ -6,7 +7,10 @@ port = 20001
 Socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 Socket.bind((local_ip, port))
 
-while(True):
-    received_bytes = Socket.recvfrom(1024)
+while (True):
+    received_bytes = Socket.recvfrom(5)
     message = received_bytes[0]
-    print(message.decode())
+    message_bit = bitarray.bitarray()
+    message_bit.frombytes(message)
+
+    print(message_bit)
