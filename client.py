2ef5c55179d17088cd975e2b5569a4fc816cbe3a
magdss <62175061+magdss@users.noreply.github.com>
2021-01-05 16:22:58 +0100

    update

--- client.py
+++ client.py
@@ -1,40 +1,144 @@
 import socket
 import bitarray
 
+class Client:
+    server_ip = ("127.0.0.1", 20001)
+    Socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    session_id = 0
+
+    OK = bitarray.bitarray([False, False, False, True])
+    RETRY = bitarray.bitarray([False, False, True, True])
+
+    GET_ID = bitarray.bitarray([False, False, False, False, True])
+    SEND_L = bitarray.bitarray([False, False, False, True, False])
+    SEND_GUESS = bitarray.bitarray([False, False, False, True, True])
+    LOW_RANGE = bitarray.bitarray([False, False, True, False, False])
+    HIGH_RANGE = bitarray.bitarray([False, False, True, True, False])
+    WAIT = bitarray.bitarray([True, False, False, False, False])
+    WAIT_FOR_START = bitarray.bitarray([True, True, False, False, False])
+    AGAIN = bitarray.bitarray([True, True, False, True, True])
+
+    low_range = 0
+    high_range = 0
 
 
-server_ip = ("127.0.0.1", 20001)
-Socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-id = 0
 
-def ask_for_id():               # funkcja wysyłająca prośbę o ID
-    operation = bitarray.bitarray(5)
-    operation.setall(False)
-    operation[4] = True
-    response = bitarray.bitarray(4)
-    response.setall(False)
-    id = bitarray.bitarray(6)
-    id.setall(False)
-    data = bitarray.bitarray(10)
-    data.setall(False)
-    fill = bitarray.bitarray(4)
-    fill.setall(False)
-    control_sum = bitarray.bitarray(11)
-    control_sum.setall(False)
+    def bit_to_int(self,b):
+        i = 0
+        for bit in b:
+            i = (i << 1) | bit
+        return i
 
-    header = [operation, response, id, data, control_sum, fill]
 
-    message = bitarray.bitarray(endian='big')
-    for i in range(6):
-        message += header[i]
+    def count_bits(self,arr):
+        b = 0
+        for i in arr:
+            if(i == True or i=='1'):
+                b += 1
+        return b
 
-    print(message)
-    message = message.tobytes()
 
-    Socket.sendto(message, server_ip)
+    def bitfield(self, l, n):  # zamiana liczby na bity
+        return "{0:b}".format(l).zfill(n)
+
+
+    def ask_for_id(self):               # funkcja wysyłająca prośbę o ID
+        operation = self.GET_ID
+        response = bitarray.bitarray(4)
+        response.setall(False)
+        id = bitarray.bitarray(6)
+        id.setall(False)
+        data = bitarray.bitarray(10)
+        data.setall(False)
+        fill = bitarray.bitarray(4)
+        fill.setall(False)
+        cont = self.count_bits(operation) + self.count_bits(response) + self.count_bits(id) + self.count_bits(data)
+        control_sum = self.bitfield(cont ,11)
+
+        header = [operation, response, id, data, control_sum, fill]
+
+        message = bitarray.bitarray(endian='big')
+        for i in range(6):
+            message += header[i]
+        message = message.tobytes()
+
+        self.Socket.sendto(message, self.server_ip)
+        print("Wysłana prośba o uzyskanie ID")
+        received = self.Socket.recvfrom(5)
+        received_msg = bitarray.bitarray()
+        received_msg.frombytes(received[0])
+        my_id = received_msg[9:15]
+        my_id = self.bit_to_int(my_id)
+        self.session_id = my_id
+        print(self.session_id)
+        print("ID uzyskane od serwera: ", my_id)
+
+
+    def send_L(self):               #wysyłanie liczby L
+        l = int(input("Podaj liczbe z przedziału 1-512: "))
+        operation = self.SEND_L
+        response = bitarray.bitarray(4)
+        response.setall(False)
+        id = self.bitfield(self.session_id,6)
+        data = self.bitfield(l,10)
+        contr = self.count_bits(operation) + self.count_bits(response) + self.count_bits(id) + self.count_bits(data)
+        control_sum = self.bitfield(contr,11)
+        fill = bitarray.bitarray()
+        fill.setall(False)
+
+        header = [operation, response, id, data, control_sum, fill]
+        print(header)
+        message = bitarray.bitarray(endian='big')
+
+        for i in header:
+            message += i
+
+        message = message.tobytes()
+
+        self.Socket.sendto(message, self.server_ip)
+
+        answer = self.Socket.recvfrom(5)
+        received_msg = bitarray.bitarray()
+        received_msg.frombytes(answer[0])
+
+        if(received_msg[5:9] == self.OK):
+            print("Serwer zaakceptował twoją liczbę")
+        else:
+            print("Serwer już przyjął taką liczbę. Podaj inną liczbę.")
+            self.send_L()
+
+    def wait(self):
+        while(True):
+            received = self.Socket.recvfrom(5)
+            received_msg = bitarray.bitarray()
+            received_msg.frombytes(received[0])
+            if(received_msg[:5] == self.WAIT):
+                print("Oczekiwanie na podanie liczb przez innych graczy")
+            if(received_msg[:5] == self.WAIT_FOR_START):
+                print("Wszyscy gracze się połączyli. Oczekiwanie na start gry.")
+            if(received_msg[:5] == self.AGAIN):
+                print("Liczby podane przez graczy nie spełniają wymagań. Proszę podać kolejne liczby")
+                self.send_L()
+
+            if(received_msg[:5] == self.LOW_RANGE):
+                d = received_msg[15:25]
+                d = self.bit_to_int(d)
+                self.low_range = d
+                print("Wylosowana liczba jest większa niż " , self.low_range)
+
+            if(received_msg[:5] == self.HIGH_RANGE_RANGE):
+                d = received_msg[15:25]
+                d = self.bit_to_int(d)
+                self.high_range = d
+                print("Wylosowana liczba jest mniejsza niż ", self.high_range)
 
 
 
 
 if __name__ == "__main__" :
-    ask_for_id()
+    c = Client()
+    c.ask_for_id()
+    c.send_L()
+    c.wait()
+
+
--- server.py
+++ server.py
@@ -4,24 +4,62 @@ import random
 
 # wzory operacji
 
-GET_ID = bitarray.bitarray([False,False,False,False,True])
+GET_ID = bitarray.bitarray([False, False, False, False, True])
+SEND_L = bitarray.bitarray([False, False, False, True, False])
+SEND_GUESS = bitarray.bitarray([False, False, False, True, True])
+LOW_RANGE = bitarray.bitarray([False, False, True, False, False])
+HIGH_RANGE = bitarray.bitarray([False, False, True, True, False])
+WAIT = bitarray.bitarray([True, False, False, False, False])
+WAIT_FOR_START = bitarray.bitarray([True, True, False, False, False])
+AGAIN = bitarray.bitarray([True, True, False, True, True])
+START = bitarray.bitarray([True, True, True, False, False])
 
 
-def bitfield(n):  # zamiana liczby na bity
-    return [int(digit) for digit in bin(n)[2:]]
+#wzory odpowiedzi
+
+OK = bitarray.bitarray([False, False, False, True])
+RETRY = bitarray.bitarray([False, False, True, True])
+NOT_WIN = bitarray.bitarray([False, False, True, False])
+WIN = bitarray.bitarray([True, True, True, True])
+
+
+def bitfield(l, n):  # zamiana liczby na bity
+    return "{0:b}".format(l).zfill(n)
+
+
+def bit_to_int(b):
+    i = 0
+    for bit in b:
+        i = (i << 1) | bit
+    return i
+
+
+def count_bits(arr):
+    b = 0
+    for i in arr:
+        if(i == True):
+            b += 1
+    return b
+
+
+
 
 
 clients_id = []             # lista id klientów
-
+adresses = []
+L = []
+value = 0
 local_ip = "127.0.0.1"
 port = 20001
-
+print("Startowanie serwera .........")
 Socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 Socket.bind((local_ip, port))
 
 while(True):
     received_bytes = Socket.recvfrom(5) # odebranie wiadomości
     address = received_bytes[1]     # pobranie adresu nadawcy informacji
+    if(address not in adresses):
+        adresses.append(address)
     message = received_bytes[0]
     message_bit = bitarray.bitarray()
     message_bit.frombytes(message)     # zamiana wiadomości na bity
@@ -34,7 +72,7 @@ while(True):
     control_sum = message_bit[25:36]
     fill = message_bit[36:]
 
-    print("Received message from:", id, "is", message_bit)
+    print("Otrzymana wiadomosc od klienta o ID:", bit_to_int(id), "to ", message_bit)
 
     if(operation == GET_ID):     # sprawdzenie czy klient prosi o przydzielenie ID
         while(True):             # losowanie unikalnego ID (nieprzypisanego  do innego klienta)
@@ -42,12 +80,109 @@ while(True):
             if(new_id not in clients_id ):
                 break
         clients_id.append(new_id)     # dodanie nowego ID do listy klientów
+        print("Podłączono nowego klienta. ID nowego klienta to: ", new_id)
 
-        print(new_id)
-        new_id = "{0:b}".format(new_id).zfill(6)
+        new_id = bitfield(new_id,6)
         id_to_send = bitarray.bitarray()
         id_to_send += new_id   # zamiana id na bity
-        print(id_to_send)
+        contr = count_bits(operation) + count_bits(OK) + count_bits(id_to_send) + count_bits(data)
+        control_sum = bitfield(contr,11)
+        header = [operation, OK, id_to_send, data, control_sum, fill]
+        message_to_send = bitarray.bitarray(endian='big')
+        for i in range(5):
+            message_to_send += header[i]
+
+        message_to_send = message_to_send.tobytes()
+        Socket.sendto(message_to_send, address)
+
+    if(operation == SEND_L):
+        l = bit_to_int(data)
+        if(l not in L):
+            L.append(l)
+            contr = count_bits(operation) + count_bits(OK) + count_bits(id) + count_bits(data)
+            control_sum = bitfield(contr, 11)
+            header = [SEND_L, OK, id, data, control_sum, fill]
+            message_to_send = bitarray.bitarray(endian='big')
+            for i in range(5):
+                message_to_send += header[i]
+
+            message_to_send = message_to_send.tobytes()
+            Socket.sendto(message_to_send, address)
+            if(len(L)<3):
+                dt = bitarray.bitarray(10)
+                dt.setall(False)
+                contr = count_bits(WAIT) + count_bits(OK) + count_bits(id) + count_bits(dt)
+                control_sum = bitfield(contr,11)
+                header = [WAIT, OK, id, dt, control_sum, fill]
+                message_to_send = bitarray.bitarray(endian='big')
+                for i in range(5):
+                    message_to_send += header[i]
+
+                message_to_send = message_to_send.tobytes()
+                Socket.sendto(message_to_send, address)
+            if(len(L)>=3):
+                L.sort(reverse=True)
+                L1 = L[0]-L[2]
+                L2 = L[1]+L[2]
+                if(abs(L1-L2)<10):
+                    dt = bitarray.bitarray(10)
+                    dt.setall(False)
+                    L = []
+                    for u,a in zip(clients_id, adresses):
+                        id = bitfield(u,6)
+                        contr = count_bits(AGAIN) + count_bits(OK) + count_bits(id) + count_bits(dt)
+                        control_sum = bitfield(contr,11)
+                        header = [AGAIN, OK, id, dt, control_sum, fill]
+                        message_to_send = bitarray.bitarray(endian='big')
+                        for i in range(5):
+                            message_to_send += header[i]
+
+                        message_to_send = message_to_send.tobytes()
+                        Socket.sendto(message_to_send, a)
+                else:
+                    value = random.randint(L1, L2)
+                    dt = bitfield(L1,10)
+                    for u,a in zip(clients_id, adresses):
+                        id = bitfield(u,6)
+                        contr = count_bits(LOW_RANGE) + count_bits(OK) + count_bits(id) + count_bits(dt)
+                        control_sum = bitfield(contr,11)
+                        header = [LOW_RANGE, OK, id, dt, control_sum, fill]
+                        message_to_send = bitarray.bitarray(endian='big')
+                        for i in range(5):
+                            message_to_send += header[i]
+
+                        message_to_send = message_to_send.tobytes()
+                        Socket.sendto(message_to_send, a)
+                    dt = bitfield(L2,10)
+                    for u,a in zip(clients_id, adresses):
+                        id = bitfield(u,6)
+                        contr = count_bits(HIGH_RANGE) + count_bits(OK) + count_bits(id) + count_bits(dt)
+                        control_sum = bitfield(contr,11)
+                        header = [HIGH_RANGE, OK, id, dt, control_sum, fill]
+                        message_to_send = bitarray.bitarray(endian='big')
+                        for i in range(5):
+                            message_to_send += header[i]
+
+                        message_to_send = message_to_send.tobytes()
+                        Socket.sendto(message_to_send, a)
+
+
+
+
+        else:
+            contr = count_bits(operation) + count_bits(RETRY) + count_bits(id) + count_bits(data)
+            control_sum = bitfield(contr, 11)
+            header = [SEND_L, RETRY, id, data, control_sum, fill]
+            message_to_send = bitarray.bitarray(endian='big')
+            for i in range(5):
+                message_to_send += header[i]
+
+            message_to_send = message_to_send.tobytes()
+            Socket.sendto(message_to_send, address)
+
+
+
+
 
 
 
